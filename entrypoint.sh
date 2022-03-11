ce24ff3781bfa8173479d070eeb4d33fdd0908d8
strpc <renat.kruchinskij@gmail.com>
2022-03-11 12:05:59 +0300

    fix: run in docker, docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 APP_HOST=0.0.0.0
 APP_PORT=${APP_PORT:-8000}
-LOG_LEVEL=${LOG_LEVEL:-info}
+LOG_LEVEL=${LOG_LEVEL:-INFO}
+
+SERVER_LOG_LEVEL=$(echo ${LOG_LEVEL} | tr "[:upper:]" "[:lower:]")
 
 prod() {
-  exec uvicorn app.main:HTTPApp.create_app --host ${APP_HOST} --port ${APP_PORT} \
-  --log-level ${LOG_LEVEL} --no-use-colors --no-access-log --factory
-
+  exec uvicorn \
+  --factory app.main:HTTPApp.create_app \
+  --host ${APP_HOST} --port ${APP_PORT} \
+  --log-level ${SERVER_LOG_LEVEL} \
+  --no-use-colors \
+  --no-access-log
 }
 
 dev() {
-  exec uvicorn app.main:HTTPApp.create_app --host ${APP_HOST} --port ${APP_PORT} --reload --factory
+  exec uvicorn --factory app.main:HTTPApp.create_app \
+  --host ${APP_HOST} --port ${APP_PORT} \
+  --reload
 }
-
 
 case "$1" in
   prod)
